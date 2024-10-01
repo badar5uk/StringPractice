1ba5959fc3c451d983698103ee701e9078c2b63d
bader <bader5uk@hotmail.com>
2024-10-01 11:21:22 +0400

    user input, checks for string occurence and removes it from orignal string

---
+++ withoutString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class withoutString {
+    /*
+    Input: A user String input and another substring input
+    Output; the original string with al instances of the substring removed
+    Process: - Take a string and substring using scanner object
+             - use .replace method to remove the string from the original
+             - assign new string that would contain edited string
+     */
+
+    public static void main(String[] args) {
+        Scanner baseScanner = new Scanner(System.in);
+        System.out.println("Please enter a String");
+        String baseUSerInp = baseScanner.nextLine();
+        System.out.println("Please enter a substring to be removed fro the original string");
+        Scanner removeScanner = new Scanner(System.in);
+        String removeUserInp = removeScanner.nextLine();
+
+        if(baseUSerInp.contains(removeUserInp)){
+            String resultWithRemovedSubstring = baseUSerInp.replace(removeUserInp,"");
+            System.out.println("Your string is "+baseUSerInp +" and your removed string is "+ resultWithRemovedSubstring);
+        }
+        else{
+            System.out.println("The substring "+ removeUserInp+ " does not show up in the original string " + baseUSerInp);
+        }
+    }
+}
+
